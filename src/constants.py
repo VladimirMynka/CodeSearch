c81ca18fc77eed515d4a52996de09a4120c4e7aa
VladimirMynka <vladimirmynka@yandex.ru>
2022-12-13 03:19:59 +0700

    Started work with dataset preparation

---
+++ src/constants.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+
+SOURCE_CODE_PATH = Path("/Users/79138/Mynka/hw/2. second grade/oop/myCs")
---
+++ src/create_dataset.py
@@ -0,0 +1,5 @@
+import os
+
+from constants import SOURCE_CODE_PATH
+
+print(SOURCE_CODE_PATH.is_dir())
